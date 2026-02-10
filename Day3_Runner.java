6687200dbc5ecbf06b3e4b6f978884de9f235f67
Your Name <you@example.com>
2026-02-10 15:00:54 +0300

    Day 3: Add Game and Day3_Runner classes

---
+++ Day3_Runner.java
@@ -0,0 +1,9 @@
+public class Day3_Runner {
+    public static void main(String[] args) {
+        // TODO: This file is done! Look how clean 'main' is now.
+        // It doesn't know HOW the game works. It just knows how to start it.
+        
+        Game myGame = new Game();
+        myGame.start();
+    }
+}
---
+++ Game.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Game {
+    // Fields (Data specific to this object)
+    private String playerName;
+    private int secretNumber;
+    private int attempts;
+    private Scanner scanner;
+
+    // Constructor (Sets up the data)
+    public Game() {
+        this.scanner = new Scanner(System.in);
+        
+        // Ask for the player's name
+        System.out.print("Enter your name: ");
+        this.playerName = scanner.nextLine();
+        
+        // Initialize secretNumber (1-100) using Random
+        Random rand = new Random();
+        this.secretNumber = rand.nextInt(100) + 1;
+        this.attempts = 0;
+    }
+
+    // Method (Behavior)
+    public void start() {
+        System.out.println("\n--- Starting New Game ---");
+        System.out.println("Hello " + this.playerName + "! I have picked a number between 1 and 100.");
+        
+        boolean keepPlaying = true;
+        while(keepPlaying) {
+            System.out.print("Enter your guess: ");
+            int guess = scanner.nextInt();
+            this.attempts++;
+            
+            if (guess > this.secretNumber) {
+                System.out.println("Too high!");
+            } else if (guess < this.secretNumber) {
+                System.out.println("Too low!");
+            } else {
+                System.out.println("Correct, " + this.playerName + "! It took you " + this.attempts + " attempts.");
+                keepPlaying = false;
+            }
+        }
+    }
+}
